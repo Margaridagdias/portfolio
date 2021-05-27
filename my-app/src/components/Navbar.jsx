89e6c77feefc28573a695d40004b138fbe076e00
Margarida <margarida97pt@gmail.com>
2021-05-27 18:08:20 +0100

    Navbar problem

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
-  const [dropdown, setDropdown] = useState(false);
 
   const handleClick = () => setClick(!click);
 
