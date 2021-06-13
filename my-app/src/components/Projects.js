6e3a719e3b715b2e5fef72a992fd180eed413424
Margarida <margarida97pt@gmail.com>
2021-06-13 18:02:35 +0100

    Projects2

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,9 +1,34 @@
 import React from "react";
+import "../components/Projects.css";
 
 const Projects = () => {
   return (
-    <section className="qualification section">
-      <h2 className="section__title">Projects Education</h2>
+    <section className="projects section" id="projects">
+      <h2 className="section__title">Projects</h2>
+      <span className="section__subtitle">Most recent work</span>
+
+      <div className="projects__container container">
+        <div>
+          <div className="projects__content grid">
+            <img
+              src="/images/moshifypic.png"
+              alt=""
+              className="projects__img"
+            />
+            <div className="projects__data">
+              <h3 className="projects__title">
+                <p className="portfolio__description">Website that does this</p>
+                <a
+                  href="https://mdmoshify.netlify.app"
+                  className="button button-flex button--small project__button"
+                >
+                  Demo<i class="fas fa-arrow-right button-icon-1"></i>
+                </a>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
